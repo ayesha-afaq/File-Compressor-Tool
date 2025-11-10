2a172deee85c5958d09027468fd15fe3884c61ef
Areeba Javed <areeba.javaid2005@gmail.com>
2025-11-10 21:57:28 +0500

    helper functions

--- huffman.py
+++ huffman.py
@@ -57,3 +57,42 @@ class HuffmanCompressor:
         
         self.build_codes(node.left, current_code + "0")
         self.build_codes(node.right, current_code + "1")
+    def get_encoded_data(self, data):
+        """Convert bytes to encoded binary string"""
+        encoded_text = ""
+        for byte_val in data:
+            encoded_text += self.codes[byte_val]
+        return encoded_text
+    
+    def pad_encoded_text(self, encoded_text):
+        """Pad encoded text to make it multiple of 8 bits"""
+        extra_padding = 8 - (len(encoded_text) % 8)
+        if extra_padding == 8:
+            extra_padding = 0
+        encoded_text += '0' * extra_padding
+        
+        # Store padding info at beginning (3 bits is enough for 0-7)
+        padded_info = "{0:08b}".format(extra_padding)
+        encoded_text = padded_info + encoded_text
+        return encoded_text
+    
+    def get_byte_array(self, padded_encoded_text):
+        """Convert binary string to byte array"""
+        b = bytearray()
+        for i in range(0, len(padded_encoded_text), 8):
+            byte_segment = padded_encoded_text[i:i+8]
+            b.append(int(byte_segment, 2))
+        return b
+    def remove_padding(self, padded_encoded_text):
+        """Remove padding from encoded text"""
+        # Read 3 bits for padding info
+        padded_info = padded_encoded_text[:8]
+        extra_padding = int(padded_info, 2)
+        
+        padded_encoded_text = padded_encoded_text[8:]
+        if extra_padding > 0:
+            encoded_text = padded_encoded_text[:-extra_padding]
+        else:
+            encoded_text = padded_encoded_text
+        
+        return encoded_text
